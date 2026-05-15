6cd8851a8c30423d8a3a300334f6dc44da28ba26
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-05-15 10:03:36 +0900

    feature: info 자기소개 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,6 +20,15 @@ export default async function Home({
     <>
       <ProgressBar />
       <section className="p-6 md:p-10">
+        <div className="p-5 mb-10 border-l-2 border-l-point bg-foreground/10 font-bold break-keep">
+          안녕하세요. 4년간 웹 퍼블리셔로 다양한 화면을 구현하며 프론트엔드
+          개발에 관심을 갖게 되었고, React와 TypeScript를 중심으로 개발 역량을
+          확장해왔습니다. <br />
+          개인 프로젝트와 협업 프로젝트를 통해 상태 관리, API 연동, 컴포넌트
+          설계 등을 직접 구현하며 서비스 단위의 개발 경험을 쌓았습니다. <br />
+          문제를 구조적으로 분석하고, 유지보수성과 사용자 경험을 함께 고려하는
+          프론트엔드 개발자를 목표로 하고 있습니다.
+        </div>
         <div>
           <Tab current={tabWay} />
           <Suspense fallback={<Loading />}>
